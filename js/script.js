9f78540f2215f9ef408895d8a0610db1cd01f231
Dorota Zielinska <dodamad3@gmail.com>
2023-02-12 18:12:35 +0000

    initial commits

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-{
+
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 const themeName = document.querySelector(".themeName");
 
 button.addEventListener("click", () => {
+
+    
     body.classList.toggle("dark");
 
     themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
 });
 
-console.log("Hello World!");
-}
